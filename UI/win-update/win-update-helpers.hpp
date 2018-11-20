905695a9207ca007b11541fc2f5d3e4dce896c5f
18059104@qq.com <huijia264>
2018-11-20 21:38:12 +0800

    还原obs的升级代码

--- UI/win-update/win-update-helpers.hpp
+++ UI/win-update/win-update-helpers.hpp
@@ -69,34 +69,38 @@ public:
 
 /* ------------------------------------------------------------------------ */
 
-class OBSJson {
+class Json {
 	json_t *json;
 
 public:
-	inline OBSJson() : json(nullptr) {}
-	explicit inline OBSJson(json_t *json_) : json(json_) {}
-	inline OBSJson(const OBSJson &from) : json(json_incref(from.json)) {}
-	inline OBSJson(OBSJson &&from) : json(from.json) {from.json = nullptr;}
+	inline Json() : json(nullptr) {}
+	explicit inline Json(json_t *json_) : json(json_) {}
+	inline Json(const Json &from) : json(json_incref(from.json)) {}
+	inline Json(Json &&from) : json(from.json) {from.json = nullptr;}
 
-	inline ~OBSJson() {
-		if (json) json_decref(json);
+	inline ~Json() {
+		if (json)
+			json_decref(json);
 	}
 
-	inline OBSJson &operator=(json_t *json_)
+	inline Json &operator=(json_t *json_)
 	{
-		if (json) json_decref(json);
+		if (json)
+			json_decref(json);
 		json = json_;
 		return *this;
 	}
-	inline OBSJson &operator=(const OBSJson &from)
+	inline Json &operator=(const Json &from)
 	{
-		if (json) json_decref(json);
+		if (json)
+			json_decref(json);
 		json = json_incref(from.json);
 		return *this;
 	}
-	inline OBSJson &operator=(OBSJson &&from)
+	inline Json &operator=(Json &&from)
 	{
-		if (json) json_decref(json);
+		if (json)
+			json_decref(json);
 		json = from.json;
 		from.json = nullptr;
 		return *this;
@@ -106,10 +110,12 @@ public:
 
 	inline bool operator!() const {return !json;}
 
-	inline const char *GetString(const char *name, const char *def = nullptr) const
+	inline const char *GetString(const char *name,
+			const char *def = nullptr) const
 	{
 		json_t *obj(json_object_get(json, name));
-		if (!obj) return def;
+		if (!obj)
+			return def;
 		return json_string_value(obj);
 	}
 	inline int64_t GetInt(const char *name, int def = 0) const
