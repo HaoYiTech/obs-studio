ca2809e5ca805026b7e100d9f97528bf8589596f
18059104@qq.com <huijia264>
2019-01-05 23:19:27 +0800

    正在搭建小程序界面；

---
+++ wechat/qidi/app.js
@@ -0,0 +1,22 @@
+//app.js
+App({
+  onLaunch: function (options) {
+    // 打印引导参数...
+    //console.log(options)
+    // 获取系统信息同步接口...
+    this.globalData.m_sysInfo = wx.getSystemInfoSync()
+    // 展示本地存储能力
+    /*var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)*/
+  },
+  doShowLoading: function() {
+    wx.showLoading({ title: '加载中' })
+  },
+  globalData: {
+    m_urlPrev: 'https://myhaoyi.com/wxapi.php/',
+    m_userInfo: null,
+    m_sysInfo: null,
+    m_nUserID: 0
+  }
+})
