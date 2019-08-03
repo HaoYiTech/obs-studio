bfb3f7ab51dc67862ee1be597797253b224b0448
18059104@qq.com <huijia264>
2019-08-03 15:14:51 +0800

    升级版本到1.2.12

--- Screen/update-app.h
+++ Screen/update-app.h
@@ -6,7 +6,7 @@
 #define WIN_MANIFEST_URL    DEF_WEB_CLASS "/update_studio/screen/manifest.json"
 #define WIN_DXSETUP_URL     DEF_WEB_CLASS "/update_studio/dxwebsetup.exe"
 #define WIN_UPDATER_URL     DEF_WEB_CLASS "/update_studio/updater.exe"
-#define APP_DXSETUP_PATH    "obs-teacher\\updates\\dxwebsetup.exe"
+#define APP_DXSETUP_PATH    "obs-screen\\updates\\dxwebsetup.exe"
 #define APP_MANIFEST_PATH   "obs-screen\\updates\\manifest.json"
 #define APP_UPDATER_PATH    "obs-screen\\updates\\updater.exe"
 #define APP_NAME            L"screen"
