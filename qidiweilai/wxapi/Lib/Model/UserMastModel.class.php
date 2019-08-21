2059a9b346d4b0190cf9acf85060419fb404140e
18059104@qq.com <huijia264>
2019-08-21 19:15:09 +0800

    更新编号错位问题；

--- qidiweilai/wxapi/Lib/Model/UserMastModel.class.php
+++ qidiweilai/wxapi/Lib/Model/UserMastModel.class.php
@@ -3,8 +3,8 @@
 class UserMastModel extends ViewModel {
     public $viewFields = array(
         'User'=>array('*','_type'=>'LEFT'), //Must be LEFT 返回左表所有数据，即使右表没有匹配的
-        'Shop'=>array('shop_id','master_id'=>'master_shop_id','_on'=>'User.user_id=Shop.master_id','_type'=>'LEFT'),
-        'Agent'=>array('agent_id','master_id'=>'master_agent_id','_on'=>'User.user_id=Agent.master_id')
+        'Shop'=>array('shop_id'=>'master_shop_id','_on'=>'User.user_id=Shop.master_id','_type'=>'LEFT'),
+        'Agent'=>array('agent_id'=>'master_agent_id','_on'=>'User.user_id=Agent.master_id')
     );
 }
 ?>
